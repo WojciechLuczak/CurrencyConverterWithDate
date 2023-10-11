76781b82c366f80742c415ae9b63fcc2a986859f
Wojciech Luczak <wojtek@alphabeteventz.pl>
2023-10-11 21:50:15 +0200

    added useEffect to Clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,5 +2,15 @@ import React, {useState, useEffect} from "react";
 import "./style.css";
 
 export const Clock = () => {
+    const [date, setDate] = useState(new Date());
 
+    useEffect(() => {
+        const intervalId = setInterval(() =>{
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    });
 };
